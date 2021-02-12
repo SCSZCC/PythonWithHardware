742aa9446c55362df932802f74d40266c1bc6bec
SCSZCC <36698850+SCSZCC@users.noreply.github.com>
2021-02-12 11:19:23 +0800

    0.1
    0.1

---
+++ CyberPi/Python with CyberPi 088（童芯派 新年好） .py
@@ -0,0 +1,51 @@
+""""
+名称：088 童芯派 新年快乐
+硬件： 童芯派
+功能介绍：
+就是用童芯派编写了一个《新年好》的曲子，因为今天是大年初一，原本想昨晚写的。
+水了一个程序。
+
+
+难度：⭐⭐⭐
+支持的模式：上传模式、在线模式
+无
+
+"""
+# ---------程序分割线----------------程序分割线----------------程序分割线----------
+
+import cyberpi
+import time
+
+while True:
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(55, 0.75)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(67, 0.25)
+        cyberpi.audio.play_music(67, 0.25)
+        cyberpi.audio.play_music(65, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(62, 0.25)
+        time.sleep(0.5)
+        cyberpi.audio.play_music(62, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(65, 0.25)
+        cyberpi.audio.play_music(65, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(62, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(60, 0.25)
+        cyberpi.audio.play_music(64, 0.25)
+        cyberpi.audio.play_music(62, 0.25)
+        cyberpi.audio.play_music(55, 0.75)
+        cyberpi.audio.play_music(59, 0.75)
+        cyberpi.audio.play_music(62, 0.25)
+        cyberpi.audio.play_music(60, 0.75)
+        time.sleep(1)
