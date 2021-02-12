79efb12cc283b9f3dd5c92734e3f890a61cb58a5
SCSZCC <36698850+SCSZCC@users.noreply.github.com>
2021-02-12 11:26:48 +0800

    0.1
    0.1

--- CyberPi/Python with CyberPi 089（童芯派 mBuild点阵屏 计时装置）.py
+++ CyberPi/Python with CyberPi 089（童芯派 mBuild点阵屏 计时装置）.py
@@ -5,6 +5,15 @@
 使用点阵屏实现可视化的计时效果,按下A键以点亮点阵屏的方式进行计时，
 按下B键以熄灭点阵屏的方式进行计时。
 
+使用到的API及功能解读：
+1.cyberpi.led_matrix.on(x, y, 1)
+点亮点阵屏指定x、y坐标的led灯
+
+2.cyberpi.led_matrix.off(x, y, 1)
+熄灭点阵屏指定x、y坐标的led灯
+
+3.cyberpi.led_matrix.clear(1)
+熄灭点阵屏所有的LED灯
 
 难度：⭐⭐⭐⭐
 支持的模式：上传模式、在线模式
