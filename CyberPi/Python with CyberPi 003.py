f52ec572dec9a6ce59567e71069f283c36b09471
SCSZCC <36698850+SCSZCC@users.noreply.github.com>
2021-02-14 10:14:49 +0800

    0.1
    0.1

--- CyberPi/Python with CyberPi 003.py
+++ CyberPi/Python with CyberPi 003.py
@@ -1,32 +1,35 @@
 """"
-名称：002 屏幕上的文字
+名称：003 声音播放器
 硬件：
-功能介绍：在童芯派的屏幕上显示文字
-
+功能介绍：让童芯派播放音乐
 难度：⭐
-
-支持的模式：上传、在线
+支持的模式：上传、在线都支持
 
 使用到的API及功能解读：
-1.cyberpi.console.print('char')
-  童芯派的.控制台（屏幕）.打印文字（'要打印的文字内容'）
-
-2.cyberpi.display.clear()
-  童芯派的.显示.清空（）
-
+1.cyberpi.audio.play_until('hello')
+  童芯派的.音效.播放预置音效到结束（'hello'）
+  更多音效名称请查阅童芯派API文档
+2.cyberpi.audio.play_music(note, beat, type='piano')
+  童芯派的.音效.播放乐器音效（音调，节拍，乐器类型为：钢琴）
+  使用示例：cyberpi.audio.play_music(67, 0.25, 'piano')
 """
+
 # ---------程序分割线----------------程序分割线----------------程序分割线----------
+import cyberpi  # 导入童芯派的第三方库
 
-import cyberpi                                  # 导入童芯派的第三方库
-
-cyberpi.display.clear()                         # 清空屏幕上的内容，对屏幕进行初始化
-cyberpi.console.print('一起来学Python吧！')       # 在屏幕上打印显示  一起来学Python吧！
-cyberpi.console.print('加油！')                  # 在屏幕上打印显示：“加油”！
-
-
-
+cyberpi.audio.play_until("hello")
+cyberpi.audio.play_music(60, 0.25, 'piano')
+cyberpi.audio.play_music(60, 0.25, 'piano')
+cyberpi.audio.play_music(67, 0.25, 'piano')
+cyberpi.audio.play_music(67, 0.25, 'piano')
+cyberpi.audio.play_music(69, 0.25, 'piano')
+cyberpi.audio.play_music(69, 0.25, 'piano')
+cyberpi.audio.play_music(67, 0.5, 'piano')
 
 # 拓展：
-#    1.将代码中的print更换成println，观察效果上的变化。
-#    2.如果删除cyberpi.display.clear(),再次运行程序，观察屏幕的显示变化。
+# 1.你可以将根据以下的音调完成小星星的程序编写吗？
+# 60 60 67 67 69 69 67  65 65 64 64 62 62 60    67 67 65 65 64 64 62
+# 67 67 65 65 64 64 62  60 60 67 67 69 69 67    65 65 64 64 62 62 60
+# 每小节最后一个音符为0.5拍
+# 2.如果将cyberpi.audio.play_until('hello')更换成 cyberpi.audio.play('hello')效果有什么变化？
 
